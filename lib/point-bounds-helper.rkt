b9ba8d2f981007e46eab4ea42bbed662046cbb3a
Mathew Weitzel <mweitzel@gmail.com>
2016-09-28 14:30:45 -0400

    add beginnings of line instersection library

--- lib/point-bounds-helper.rkt
+++ lib/point-bounds-helper.rkt
@@ -1,5 +1,69 @@
 #lang racket
 
-(define (within_bounds? point boundaries) true)
+(define (within_bounds? point boundaries)
+  (write point) (newline)
+  )
+
+
+; see http://www-cs.ccny.cuny.edu/~wolberg/capstone/intersection/Intersection%20point%20of%20two%20lines.html
+; the article describes:
+;       (x4 - x3)(y1 - y3) - (y4 - y3)(x1 - x3)
+; ua =  ---------------------------------------
+;       (y4 - y3)(x2 - x1) - (x4 - x3)(y2 - y1)
+;
+;       (x2 - x1)(y1 - y3) - (y2 - y1)(x1 - x3)
+; ub =  ---------------------------------------
+;       (y4 - y3)(x2 - x1) - (x4 - x3)(y2 - y1)
+
+; with line segments described as '(x y x y)
+; and using the following mapping to our data structures
+;    our data     article convention
+; a '(x y x y)    x1 y1 x2 y2
+; b '(x y x y)    x3 y3 x4 y4
+;
+; we will use:
+;       (b3 - b1)(a2 - b2) - (b4 - b2)(a1 - b1)
+; ua =  ---------------------------------------
+;       (b4 - b2)(a3 - a1) - (b3 - b1)(a4 - a2)
+;
+;       (a3 - a1)(a2 - b2) - (a4 - a2)(a1 - b1)
+; ub =  ---------------------------------------
+;       (b4 - b2)(a3 - a1) - (b3 - b1)(a4 - a2)
+
+
+; here I expect data to be  '(ax1 ay1 ax2 ay2) '(bx1 by1 bx2 by2)
+; essentially flipping all 
+; even though article labels suggest '(ax1 ax2 ay1 ay2) '(bx1 bx2 by1 by2), which we don't use
+
+(define (ua_numerator a b) ; (b3 - b1)(a2 - b2) - (b4 - b2)(a1 - b1)
+  (-
+    (*
+      (- (third b) (first b))
+      (- (second a) (second b)))
+    (*
+      (- (fourth b) (second b))
+      (- (first a) (first b)))))
+
+(define (ub_numerator a b) ; (a3 - a1)(a2 - b2) - (a4 - a2)(a1 - b1)
+  (-
+    (*
+      (- (third a) (first a))
+      (- (second a) (second b)))
+    (*
+      (- (fourth a) (second b))
+      (- (first a) (first b)))))
+
+(define (_denomenator a b) ; (b4 - b2)(a3 - a1) - (b3 - b1)(a4 - b2)
+  (-
+    (*
+      (- (fourth b) (second b))
+      (- (third a) (first a)))
+    (*
+      (- (third a) (first b))
+      (- (fourth a) (second a)))))
+
 
 (provide within_bounds?)
+(provide ua_numerator)
+(provide ub_numerator)
+(provide _denomenator)
--- test/test-point-bounds-helper.rkt
+++ test/test-point-bounds-helper.rkt
@@ -3,7 +3,22 @@
 (require "assert.rkt")
 (require "../lib/point-bounds-helper.rkt")
 
-(assert "finds point is within a square"
-  (within_bounds?
-    '(1 1)
-    '('(0 0) '(0 2) '(2 2) '(2 0))))
+; (assert "finds point is within a square"
+;   (within_bounds?
+;     '(1 1)
+;     '('(0 0) '(0 2) '(2 2) '(2 0))))
+
+; (refute "finds point is outside a square"
+;   (within_bounds?
+;     '(-1 -1)
+;     '('(0 0) '(0 2) '(2 2) '(2 0))))
+
+(define list_a '(0 0 1 1))
+(define list_b '(0 1 1 0))
+
+(assert "ua_numerator" (equal? -1 (ua_numerator list_a list_b)))
+(assert "ub_numerator" (equal? -1 (ub_numerator list_a list_b)))
+(assert "_denomenator" (equal? -2 (_denomenator list_a list_b)))
+(assert "ua_numerator is 0 for coincident lists" (equal? 0 (ua_numerator list_a list_a)))
+(assert "ub_numerator is 0 for coincident lists" (equal? 0 (ub_numerator list_a list_a)))
+(assert "_denomenator is 0 if lists are parallel" (equal? 0 (_denomenator list_a list_a)))
