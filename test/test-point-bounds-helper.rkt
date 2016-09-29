a21e47b76b212e67e28966391368639e69fef025
Mathew Weitzel <mweitzel@gmail.com>
2016-09-28 22:28:35 -0400

    uncomment initial driving tests

--- test/test-point-bounds-helper.rkt
+++ test/test-point-bounds-helper.rkt
@@ -3,15 +3,15 @@
 (require "assert.rkt")
 (require "../lib/point-bounds-helper.rkt")
 
-; (assert "finds point is within a square"
-;   (within_bounds?
-;     '(1 1)
-;     '('(0 0) '(0 2) '(2 2) '(2 0))))
+(assert "finds point is within a square"
+  (within_bounds?
+    '(1 1)
+    (list '(0 0) '(0 2) '(2 2) '(2 0))))
 
-; (refute "finds point is outside a square"
-;   (within_bounds?
-;     '(-1 -1)
-;     '('(0 0) '(0 2) '(2 2) '(2 0))))
+(refute "finds point is outside a square"
+  (within_bounds?
+    '(-1 -1)
+    (list '(0 0) '(0 2) '(2 2) '(2 0))))
 
 (assert "point is within bounds"
   (within_bounds?                                           ;   1---------4
